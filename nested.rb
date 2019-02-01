5a17095d8b2b5d734682477ded1c993277ec98c9
Eyal Toledano <eyaltoledano@me.com>
2019-02-01 03:53:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,6 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 assembly = "Assembly"
 		programmer_hash[:dennis_ritchie][:languages].unshift("Assembly")
 		programmer_hash
 end
